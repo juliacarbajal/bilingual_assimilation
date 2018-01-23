a3eea196493595e80c2ebf816d3f7dace3ec82a3
Julia C <carbajal.mjulia@gmail.com>
2018-01-23 10:54:27 +0100

    Cleaned up analysis script (moved exploratory analyses to a separate script).

---
+++ scripts/additional_exploratory_plots_and_analyses.R
@@ -0,0 +1,154 @@
+# Additional exploratory plots and analyses (not pre-registered)
+
+
+# WILCOXON TESTS FOR PLACE AND VOICING EFFECT ####
+
+# Using Wilcoxon test because the distributions are not normal (bounded by 0) so t-test is not appropriate.
+# Note: These tests are just as reference, the full glmer model (logistic regression) should be used instead.
+
+# Wilcoxon test for voicing:
+voicing.effect = wilcox.test(perc.F ~ context, data = subset(subject.percentage.familiar, rule=='voicing'), paired = T, alternative = 'less')
+
+# Wilcoxon test for place:
+place.effect = wilcox.test(perc.F ~ context, data = subset(subject.percentage.familiar, rule=='place'), paired = T, alternative = 'less')
+
+# Exact Wilcoxon tests (using coin package):
+voicing.AU = subset(subject.percentage.familiar, rule=='voicing' & context=='AU')$perc.F
+voicing.AV = subset(subject.percentage.familiar, rule=='voicing' & context=='AV')$perc.F
+place.AU = subset(subject.percentage.familiar, rule=='place' & context=='AU')$perc.F
+place.AV = subset(subject.percentage.familiar, rule=='place' & context=='AV')$perc.F
+
+wilcoxsign_test(voicing.AV ~ voicing.AU, alternative = 'greater')
+wilcoxsign_test(place.AV ~ place.AU, alternative = 'greater')
+
+
+# ADDITIONAL PLOTS ####
+
+# 3.1 Density of viable - unviable difference for place and voicing
+ggplot(subject.diff,aes(x=diff,fill=rule))+geom_density(alpha=0.4, adjust=0.75) +
+  geom_vline(xintercept=0) +
+  xlab('diff (% viable - % unviable)') + xlim(-100,100)
+
+# 3.2 Difference per subject
+ggplot(subject.percentage.familiar,aes(x=context,y=perc.F,group=subject_id,color=as.factor(subject_id)))+
+  geom_line(position=position_jitter(w=0, h=1))+facet_wrap(~rule)
+
+
+## Barplots per child:
+
+# 4.1 Barplots per child, ordered by id
+ggplot(subject.percentage.familiar,aes(x=rule, y=perc.F, fill=context))+
+  geom_bar(colour = "black", position='dodge', stat='identity') +
+  ylim(0,100) + facet_wrap(~subject_id) +
+  xlab("Assimilation Rule") + ylab("Percentage of Familiar word choice") +
+  geom_text(aes(label = round(perc.F, digits = 1)), position = position_dodge(width = .9), vjust = -0.5)
+
+# 4.2 Barplots per child, ordered by number of training items
+ggplot(subject.percentage.familiar,aes(x=rule, y=perc.F, fill=context))+
+  geom_bar(colour = "black", position='dodge', stat='identity') +
+  ylim(0,100) + facet_wrap(~n.training + subject_id) +
+  xlab("Assimilation Rule") + ylab("Mean Percentage of Familiar word choice") +
+  geom_text(aes(label = round(perc.F, digits = 1)), position = position_dodge(width = .9), vjust = -0.5)
+
+# 4.3 Barplots per child, ordered by age
+ggplot(subject.percentage.familiar,aes(x=rule, y=perc.F, fill=context))+
+  geom_bar(colour = "black", position='dodge', stat='identity') +
+  ylim(0,100) + facet_wrap(~age + subject_id) +
+  xlab("Assimilation Rule") + ylab("Percentage of Familiar word choice") +
+  geom_text(aes(label = round(perc.F, digits = 1)), position = position_dodge(width = .9), vjust = -0.5)
+
+# 4.4 Barplots per child, ordered by test list
+ggplot(subject.percentage.familiar,aes(x=rule, y=perc.F, fill=context))+
+  geom_bar(colour = "black", position='dodge', stat='identity') +
+  ylim(0,100) + facet_wrap(~config_profile + subject_id) +
+  xlab("Assimilation Rule") + ylab("Mean Percentage of Familiar word choice") +
+  geom_text(aes(label = round(perc.F, digits = 1)), position = position_dodge(width = .9), vjust = -0.5)
+
+# 4.5 Barplots per child, ordered by training list
+ggplot(subject.percentage.familiar,aes(x=rule, y=perc.F, fill=context))+
+  geom_bar(colour = "black", position='dodge', stat='identity') +
+  ylim(0,100) + facet_wrap(~training_list + config_profile + subject_id) +
+  xlab("Assimilation Rule") + ylab("Mean Percentage of Familiar word choice") +
+  geom_text(aes(label = round(perc.F, digits = 1)), position = position_dodge(width = .9), vjust = -0.5)
+
+
+
+## Barplots per item:
+
+# 5.1 Barplots per item, rule==place
+ggplot(subset(item_means.all,rule=='place'),aes(x=context, y=mean, fill=context))+
+  geom_bar(colour = "black", position='dodge', stat='identity') +
+  ylim(0,1) + facet_wrap(~word) +
+  xlab("Context") + ylab("Mean Percentage of Familiar word choice") +
+  geom_text(aes(label = round(mean, digits = 2)), position = position_dodge(width = .9), vjust = -0.5)
+
+# 5.2 Barplots per item, rule==voicing
+ggplot(subset(item_means.all,rule=='voicing'),aes(x=context, y=mean, fill=context))+
+  geom_bar(colour = "black", position='dodge', stat='identity') +
+  ylim(0,1) + facet_wrap(~word) +
+  xlab("Context") + ylab("Percentage of Familiar word choice") +
+  theme(text = element_text(size = 16)) +
+  geom_text(aes(label = round(mean, digits = 2)), position = position_dodge(width = .9), vjust = -0.5)
+
+
+
+##### OTHER ANALYSES #####
+
+# FAMILIAR OBJECT BIAS VS AGE ####
+subject.fam.bias = data %>%
+  group_by(subject_id) %>%
+  summarise(N.familiar = sum(fam)) %>%
+  ungroup()
+
+subject.fam.bias = merge(subject.fam.bias,age_sex)
+subject.fam.bias$subject_id = as.factor(subject.fam.bias$subject_id)
+
+# Plot
+ggplot(subject.fam.bias,aes(x=age,y=N.familiar*100/24)) + geom_point() + geom_smooth(method='lm')+
+  ylim(0,100) +
+  xlab("Age (months)") +
+  ylab("Total % of familiar object responses") +
+  theme(text = element_text(size = 16))
+
+# Correlation
+cor.test(subject.fam.bias$age,subject.fam.bias$N.familiar, alternative = 'less')
+
+
+# RELATIONSHIP BETWEEN AGE AND TRAINING DIFFICULTY ####
+
+training.age = merge(training.trials,age_sex)
+ggplot(training.age,aes(x=age,y=n.training)) + geom_point()
+ggplot(training.age,aes(x=sex,y=n.training)) + geom_boxplot()
+
+# Children who failed:
+failed = subset(training.age,!(subject_id %in% subject.diff$subject_id)) %>%
+  mutate(passed = 'no')
+succeeded = subset(training.age,(subject_id %in% subject.diff$subject_id)) %>%
+  mutate(passed = 'yes')
+
+pass.data = rbind(failed,succeeded)
+ggplot(pass.data,aes(x=age,fill=passed))+geom_density(alpha=0.4) +
+  theme(text = element_text(size = 16)) +
+  xlab('Age (months)')
+
+ggplot(pass.data, aes(x=sex, fill=passed)) + geom_histogram(stat='count')
+
+# VOICING EFFECT VS AGE (There doesn't seem to be an effect of age)
+ggplot(subset(subject.diff,rule == "voicing"),aes(x=age,y=diff)) + geom_point()+geom_smooth(method='lm') +
+  xlab('Age (months)') +
+  ylab('Voicing difference (Viable - Unviable)') +
+  theme(text = element_text(size = 16))
+
+# Correlation
+cor.test(subset(subject.diff,rule == "voicing")$age,subset(subject.diff,rule == "voicing")$diff)
+
+# PLACE EFFECT VS AGE 
+ggplot(subset(subject.diff,rule == "place"),aes(x=age,y=diff)) + geom_point()+geom_smooth(method='lm')
+
+# CORRELATION BETWEEN PLACE AND VOICING EFFECT?
+# WEIRDEST THING EVER: JITTER DOESNT WORK UNLESS NUMBERS ARE ROUNDED
+ggplot(subject.diff.wide, aes(x= round(voicing,2), y=round(place,2))) +
+  geom_jitter() + geom_smooth(method = 'lm') +
+  xlim(-100,100) + ylim(-100,100)
+
+cor.test(subset(subject.diff,rule == "voicing")$diff,subset(subject.diff,rule == "place")$diff)
--- scripts/analysis.R
+++ scripts/analysis.R
@@ -1,8 +1,6 @@
 ###################################################################
 # Script for analysis of Bilingual Assimilation test
 # Created by: Julia Carbajal (LSCP - Ecole Normale Superieure) 2017
-#
-# To Do: Add trial exclusion based on vocabulary questionnaires
 ###################################################################
 
 # LOAD LIBRARIES ####
@@ -13,8 +11,10 @@ library(ggplot2)
 library(coin) # Needed for calculation of Exact Wilcoxon test
 library(car) # Needed to obtain p-values
 
+
 # Select group to analyse:
-subject.group = 0 # Set to 0 for Monolinguals, 1 for Bilinguals
+subject.group = 1 # Set to 0 for Monolinguals, 1 for Bilinguals
+
 
 # LOAD DATA ####
 if (subject.group == 0) {
@@ -50,6 +50,7 @@ source("scripts/measures.R")
 included.agesex = subset(age_sex,subject_id %in% data$subject_id)
 summary(included.agesex)
 
+
 # MAIN PLOTS ####
 
 condition_means$context = factor(condition_means$context, levels=c('AV','AU'), labels=c("viable","unviable"))
@@ -64,7 +65,7 @@ if (subject.group == 0) {
   group.colours = c('#3d98da','#c2e5ff')
 }
 
-# 1.1 Grouped barplots (rule x context, all subjects)
+# 1. Grouped barplots (rule x context, all subjects)
 ggplot(condition_means,aes(x=rule, y=mean,fill=context))+
   geom_bar(colour = "black", position='dodge',stat='identity') +
   geom_errorbar(position=position_dodge(width = .9),aes(ymin=mean-se, ymax=mean+se),width=0.2) +
@@ -72,23 +73,13 @@ ggplot(condition_means,aes(x=rule, y=mean,fill=context))+
   xlab("Assimilation Rule") +
   ylab("Percentage of Familiar object choice") +
   ggtitle(group.title) +
-  #geom_text(aes(label = round(mean, digits = 1)), position = position_dodge(width = .9), vjust = -4) +
   theme(text = element_text(size = 23)) +
   scale_fill_manual(values = group.colours) +
   theme(legend.position = c(.85, .85))
 # Note: I'm using greens and blues for my BUCLD poster. Otherwise, default colours: '#00bfc4','#ff6666'
 
-# 1.2 Barplots per counterbalancing list
-ggplot(condition_means.list,aes(x=rule, y=mean,fill=context))+
-  geom_bar(colour = "black", position='dodge',stat='identity') +
-  geom_errorbar(position=position_dodge(width = .9),aes(ymin=mean-se, ymax=mean+se),width=0.2) +
-  ylim(0,100) + facet_wrap(~config_profile) +
-  xlab("Assimilation Rule") +
-  ylab("Mean Percentage of Familiar word choice") +
-  #geom_text(aes(label = round(mean, digits = 1)), position = position_dodge(width = .9), vjust = -4) +
-  theme(text = element_text(size = 16)) 
 
-# 2.1 Grouped boxplots (rule x context, all subjects)
+# 2. Grouped boxplots (rule x context, all subjects)
 
 subject.percentage.familiar$context = factor(subject.percentage.familiar$context, levels=c('AV','AU'), labels=c("viable","unviable"))
 subject.percentage.familiar$rule = factor(subject.percentage.familiar$rule, levels=c('voicing','place'))
@@ -99,41 +90,8 @@ ggplot(subject.percentage.familiar, aes(x=rule, y=perc.F,fill=context))+
   xlab("Assimilation Rule") +
   ylab("Percentage of Familiar object choice") +
   theme(text = element_text(size = 22)) +
-  scale_fill_manual(values=c('#8fd0ff','#c2e5ff')) # This line changes the order of the colours.
+  scale_fill_manual(values = group.colours)
 # Note: I'm using greens and blues for my BUCLD poster. Otherwise, default colours: '#00bfc4','#ff6666'
-# Greens (for monolingual data): c('#5ab345','#bde19a')
-# Blues (for bilingual data): c('#8fd0ff','#c2e5ff')
-
-# 2.2 Boxplots per list
-ggplot(subject.percentage.familiar, aes(x=rule, y=perc.F,fill=context))+
-  geom_boxplot(colour = "black", position='dodge') +
-  ylim(0,100) + facet_wrap(~config_profile) +
-  xlab("Assimilation Rule") +
-  ylab("Percentage of Familiar word choice") +
-  theme(text = element_text(size = 16))
-
-
-
-# WILCOXON TESTS FOR PLACE AND VOICING EFFECT ####
-
-# Using Wilcoxon test because the distributions are not normal (bounded by 0) so t-test is not appropriate.
-# Note: These tests are just as reference, the full glmer model (logistic regression) should be used instead.
-
-# Wilcoxon test for voicing:
-voicing.effect = wilcox.test(perc.F ~ context, data = subset(subject.percentage.familiar, rule=='voicing'), paired = T, alternative = 'less')
-
-# Wilcoxon test for place:
-place.effect = wilcox.test(perc.F ~ context, data = subset(subject.percentage.familiar, rule=='place'), paired = T, alternative = 'less')
-
-# Exact Wilcoxon tests (using coin package):
-voicing.AU = subset(subject.percentage.familiar, rule=='voicing' & context=='AU')$perc.F
-voicing.AV = subset(subject.percentage.familiar, rule=='voicing' & context=='AV')$perc.F
-place.AU = subset(subject.percentage.familiar, rule=='place' & context=='AU')$perc.F
-place.AV = subset(subject.percentage.familiar, rule=='place' & context=='AV')$perc.F
-
-wilcoxsign_test(voicing.AV ~ voicing.AU, alternative = 'greater')
-wilcoxsign_test(place.AV ~ place.AU, alternative = 'greater')
-
 
 
 # LOGISTIC REGRESSION ####
@@ -147,10 +105,6 @@ wilcoxsign_test(place.AV ~ place.AU, alternative = 'greater')
 contrasts(data$rule)    = c(1,0)
 contrasts(data$context) = c(1,0)
 
-# Define interaction term (useful for stepwise model comparison tests)
-data$interaction<-contrasts(data$rule)[data$rule]*
-  contrasts(data$context)[data$context]
-
 # FULL MODEL :
 data.glm1 = glmer(fam ~ rule*context + (rule*context|subject_id) + (1+context|word), data=data, family="binomial", control=glmerControl('bobyqa'))
 summary(data.glm1)
@@ -161,6 +115,18 @@ summary(data.glm1)
 # Significance of the full model (comparison against null model):
 model.null = glmer(fam ~ 1 + (rule*context|subject_id) + (1+context|word), data=data, control=glmerControl('bobyqa'), family="binomial")
 anova(model.null,data.glm1)
+
+# P-values obtained using car package:
+Anova(data.glm1, type="III")
+
+
+# STEP-WISE MODEL COMPARISONS (not used in current analysis)
+# Note: Some of the reduced models do not converge so can't be used for all effects,
+# we're using the car package to obtain p-values instead.
+
+# Define interaction term (useful for stepwise model comparison tests)
+data$interaction<-contrasts(data$rule)[data$rule]*
+  contrasts(data$context)[data$context]
 
 # Significance of main effect of context:
 model.nocontext = glmer(fam ~ 1 + rule + interaction + (rule*context|subject_id) + (1+context|word), data=data, control=glmerControl('bobyqa'), family="binomial")
@@ -174,142 +140,10 @@ anova(model.norule,data.glm1)
 model.nointeraction = glmer(fam ~ 1 + rule + context + (rule*context|subject_id) + (1+context|word), data=data, control=glmerControl('bobyqa'), family="binomial")
 anova(model.nointeraction,data.glm1)
 
-# Alternatively (since the reduced models may not converge) using car package:
-Anova(data.glm1, type="III")
-
 
 # CHECK FOR EFFECT OF BLOCK:
-model.block = glmer(fam ~ rule*context*block + (rule*context|subject_id) + (1+context|word), data=data, family="binomial", control=glmerControl('bobyqa'))
+model.block = glmer(fam ~ rule*context+block + (rule*context|subject_id) + (1+context|word), data=data, family="binomial", control=glmerControl('bobyqa'))
 summary(model.block)
 anova(data.glm1,model.block)
 
-# ADDITIONAL PLOTS ####
 
-# 3.1 Density of viable - unviable difference for place and voicing
-ggplot(subject.diff,aes(x=diff,fill=rule))+geom_density(alpha=0.4, adjust=0.75) +
-  geom_vline(xintercept=0) +
-  xlab('diff (% viable - % unviable)') + xlim(-100,100)
-
-# 3.2 Difference per subject
-ggplot(subject.percentage.familiar,aes(x=context,y=perc.F,group=subject_id,color=as.factor(subject_id)))+
-  geom_line(position=position_jitter(w=0, h=1))+facet_wrap(~rule)
-
-
-## Barplots per child:
-
-# 4.1 Barplots per child, ordered by id
-ggplot(subject.percentage.familiar,aes(x=rule, y=perc.F, fill=context))+
-  geom_bar(colour = "black", position='dodge', stat='identity') +
-  ylim(0,100) + facet_wrap(~subject_id) +
-  xlab("Assimilation Rule") + ylab("Percentage of Familiar word choice") +
-  geom_text(aes(label = round(perc.F, digits = 1)), position = position_dodge(width = .9), vjust = -0.5)
-
-# 4.2 Barplots per child, ordered by number of training items
-ggplot(subject.percentage.familiar,aes(x=rule, y=perc.F, fill=context))+
-  geom_bar(colour = "black", position='dodge', stat='identity') +
-  ylim(0,100) + facet_wrap(~n.training + subject_id) +
-  xlab("Assimilation Rule") + ylab("Mean Percentage of Familiar word choice") +
-  geom_text(aes(label = round(perc.F, digits = 1)), position = position_dodge(width = .9), vjust = -0.5)
-
-# 4.3 Barplots per child, ordered by age
-ggplot(subject.percentage.familiar,aes(x=rule, y=perc.F, fill=context))+
-  geom_bar(colour = "black", position='dodge', stat='identity') +
-  ylim(0,100) + facet_wrap(~age + subject_id) +
-  xlab("Assimilation Rule") + ylab("Percentage of Familiar word choice") +
-  geom_text(aes(label = round(perc.F, digits = 1)), position = position_dodge(width = .9), vjust = -0.5)
-
-# 4.4 Barplots per child, ordered by test list
-ggplot(subject.percentage.familiar,aes(x=rule, y=perc.F, fill=context))+
-  geom_bar(colour = "black", position='dodge', stat='identity') +
-  ylim(0,100) + facet_wrap(~config_profile + subject_id) +
-  xlab("Assimilation Rule") + ylab("Mean Percentage of Familiar word choice") +
-  geom_text(aes(label = round(perc.F, digits = 1)), position = position_dodge(width = .9), vjust = -0.5)
-
-# 4.5 Barplots per child, ordered by training list
-ggplot(subject.percentage.familiar,aes(x=rule, y=perc.F, fill=context))+
-  geom_bar(colour = "black", position='dodge', stat='identity') +
-  ylim(0,100) + facet_wrap(~training_list + config_profile + subject_id) +
-  xlab("Assimilation Rule") + ylab("Mean Percentage of Familiar word choice") +
-  geom_text(aes(label = round(perc.F, digits = 1)), position = position_dodge(width = .9), vjust = -0.5)
-
-
-
-## Barplots per item:
-
-# 5.1 Barplots per item, rule==place
-ggplot(subset(item_means.all,rule=='place'),aes(x=context, y=mean, fill=context))+
-  geom_bar(colour = "black", position='dodge', stat='identity') +
-  ylim(0,1) + facet_wrap(~word) +
-  xlab("Context") + ylab("Mean Percentage of Familiar word choice") +
-  geom_text(aes(label = round(mean, digits = 2)), position = position_dodge(width = .9), vjust = -0.5)
-
-# 5.2 Barplots per item, rule==voicing
-ggplot(subset(item_means.all,rule=='voicing'),aes(x=context, y=mean, fill=context))+
-  geom_bar(colour = "black", position='dodge', stat='identity') +
-  ylim(0,1) + facet_wrap(~word) +
-  xlab("Context") + ylab("Percentage of Familiar word choice") +
-  theme(text = element_text(size = 16)) +
-  geom_text(aes(label = round(mean, digits = 2)), position = position_dodge(width = .9), vjust = -0.5)
-
-
-
-##### OTHER ANALYSES #####
-
-# FAMILIAR OBJECT BIAS VS AGE ####
-subject.fam.bias = data %>%
-  group_by(subject_id) %>%
-  summarise(N.familiar = sum(fam)) %>%
-  ungroup()
-
-subject.fam.bias = merge(subject.fam.bias,age_sex)
-subject.fam.bias$subject_id = as.factor(subject.fam.bias$subject_id)
-
-# Plot
-ggplot(subject.fam.bias,aes(x=age,y=N.familiar*100/24)) + geom_point() + geom_smooth(method='lm')+
-  ylim(0,100) +
-  xlab("Age (months)") +
-  ylab("Total % of familiar object responses") +
-  theme(text = element_text(size = 16))
-
-# Correlation
-cor.test(subject.fam.bias$age,subject.fam.bias$N.familiar, alternative = 'less')
-
-
-# RELATIONSHIP BETWEEN AGE AND TRAINING DIFFICULTY ####
-
-training.age = merge(training.trials,age_sex)
-ggplot(training.age,aes(x=age,y=n.training)) + geom_point()
-ggplot(training.age,aes(x=sex,y=n.training)) + geom_boxplot()
-
-# Children who failed:
-failed = subset(training.age,!(subject_id %in% subject.diff$subject_id)) %>%
-  mutate(passed = 'no')
-succeeded = subset(training.age,(subject_id %in% subject.diff$subject_id)) %>%
-  mutate(passed = 'yes')
-
-pass.data = rbind(failed,succeeded)
-ggplot(pass.data,aes(x=age,fill=passed))+geom_density(alpha=0.4) +
-  theme(text = element_text(size = 16)) +
-  xlab('Age (months)')
-
-ggplot(pass.data, aes(x=sex, fill=passed)) + geom_histogram(stat='count')
-
-# VOICING EFFECT VS AGE (There doesn't seem to be an effect of age)
-ggplot(subset(subject.diff,rule == "voicing"),aes(x=age,y=diff)) + geom_point()+geom_smooth(method='lm') +
-  xlab('Age (months)') +
-  ylab('Voicing difference (Viable - Unviable)') +
-  theme(text = element_text(size = 16))
-
-# Correlation
-cor.test(subset(subject.diff,rule == "voicing")$age,subset(subject.diff,rule == "voicing")$diff)
-
-# PLACE EFFECT VS AGE 
-ggplot(subset(subject.diff,rule == "place"),aes(x=age,y=diff)) + geom_point()+geom_smooth(method='lm')
-
-# CORRELATION BETWEEN PLACE AND VOICING EFFECT?
-# WEIRDEST THING EVER: JITTER DOESNT WORK UNLESS NUMBERS ARE ROUNDED
-ggplot(subject.diff.wide, aes(x= round(voicing,2), y=round(place,2))) +
-  geom_jitter() + geom_smooth(method = 'lm') +
-  xlim(-100,100) + ylim(-100,100)
-
-cor.test(subset(subject.diff,rule == "voicing")$diff,subset(subject.diff,rule == "place")$diff)
