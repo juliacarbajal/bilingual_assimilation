6d16f2701d99f00fe6663bf9c573d716d75087d5
Julia C <carbajal.mjulia@gmail.com>
2018-02-26 15:04:55 +0100

    Formatting figures for paper.

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -9,10 +9,13 @@ library(tidyr)
 library(lme4)
 library(ggplot2)
 library(car)
+library(ggthemes)
+library(extrafont)
+#font_import()
 
 
 # Select group to analyse:
-subject.group = 1 # Set to 0 for Monolinguals, 1 for Bilinguals
+subject.group = 0 # Set to 0 for Monolinguals, 1 for Bilinguals
 
 
 # LOAD DATA ####
@@ -47,48 +50,82 @@ source("scripts/measures.R")
 included.agesex = subset(age_sex,subject_id %in% data$subject_id)
 summary(included.agesex)
 
+# Included subjects:
+unique(data$subject_id)
+
 
 # MAIN PLOTS ####
-
-condition_means$context = factor(condition_means$context, levels=c('AV','AU'), labels=c("viable","unviable"))
-condition_means$rule = factor(condition_means$rule, levels=c('voicing','place'))
 
 # Group title (mono, bili) and colours for plotting:
 if (subject.group == 0) {
   group.title   = 'Monolinguals'
-  group.colours = c('#5ab345','#bde19a')
+  group.colours = c('gray55','gray90') #c('#5ab345','#bde19a')
 } else {
   group.title   = 'Bilinguals'
-  group.colours = c('#3d98da','#c2e5ff')
+  group.colours = c('gray55','gray90') #c('#3d98da','#c2e5ff')
+}
+
+# Defining a revised version of the Tufte theme from ggthemes (source: https://github.com/jrnold/ggthemes/issues/33)
+theme_tufte_revised <- function(base_size = 12, base_family = 'serif', ticks = TRUE) {
+  ret <- theme_bw(base_family = base_family, base_size = base_size) + 
+    theme(
+      axis.line = element_line(color = 'black'),
+      axis.title.x = element_text(vjust = -0.3), 
+      axis.title.y = element_text(vjust = 0.8),
+      axis.text.x = element_text(size = 10),
+      axis.text.y = element_text(size = 10),
+      legend.background = element_blank(), 
+      legend.key = element_blank(), 
+      legend.title = element_blank(), #element_text(face="plain"),
+      legend.position = "top",
+      legend.justification=c(0,1),
+      panel.background = element_blank(), 
+      panel.border = element_blank(),
+      panel.grid = element_blank(),
+      plot.background = element_blank(),
+      strip.background = element_blank()
+    )
+  if (!ticks) {
+    ret <- ret + theme(axis.ticks = element_blank())
+  }
+  ret
 }
 
 # 1. Grouped barplots (rule x context, all subjects)
+
+condition_means$context = factor(condition_means$context, levels=c('AV','AU'), labels=c("Viable","Unviable"))
+condition_means$rule = factor(condition_means$rule, levels=c('voicing','place'), labels = c('Voicing','Place'))
+
+png(paste("figures/barplot",group.title,".png",sep=""), width = 12, height = 10, units = 'cm', res = 300)
 ggplot(condition_means,aes(x=rule, y=mean,fill=context))+
   geom_bar(colour = "black", position='dodge',stat='identity') +
   geom_errorbar(position=position_dodge(width = .9),aes(ymin=mean-se, ymax=mean+se),width=0.2) +
-  ylim(0,100) + 
+  scale_y_continuous(limits=c(0, 100), expand = c(0, 0)) +
   xlab("Assimilation Rule") +
   ylab("Percentage of Familiar object choice") +
-  ggtitle(group.title) +
-  theme(text = element_text(size = 23)) +
+  #ggtitle(group.title) +
+  #theme(text = element_text(size = 23)) +
   scale_fill_manual(values = group.colours) +
-  theme(legend.position = c(.85, .85))
-# Note: I'm using greens and blues for my BUCLD poster. Otherwise, default colours: '#00bfc4','#ff6666'
+  theme_tufte_revised() +
+  theme(legend.position = c(.75, 1))
+dev.off()
 
 
 # 2. Grouped boxplots (rule x context, all subjects)
 
-subject.percentage.familiar$context = factor(subject.percentage.familiar$context, levels=c('AV','AU'), labels=c("viable","unviable"))
-subject.percentage.familiar$rule = factor(subject.percentage.familiar$rule, levels=c('voicing','place'))
+subject.percentage.familiar$context = factor(subject.percentage.familiar$context, levels=c('AV','AU'), labels=c("Viable","Unviable"))
+subject.percentage.familiar$rule = factor(subject.percentage.familiar$rule, levels=c('voicing','place'), labels = c('Voicing','Place'))
 
-ggplot(subject.percentage.familiar, aes(x=rule, y=perc.F,fill=context))+
+png(paste("figures/boxplot",group.title,".png",sep=""), width = 12, height = 10, units = 'cm', res = 300)
+ggplot(subject.percentage.familiar, aes(x=rule, y=perc.F, fill=context))+
   geom_boxplot(colour = "black", position='dodge') +
-  ylim(0,100) + 
-  xlab("Assimilation Rule") +
-  ylab("Percentage of Familiar object choice") +
-  theme(text = element_text(size = 22)) +
-  scale_fill_manual(values = group.colours)
-# Note: I'm using greens and blues for my BUCLD poster. Otherwise, default colours: '#00bfc4','#ff6666'
+  scale_y_continuous(limits=c(0, 100), expand = c(0, 0)) +
+  xlab("Assimilation rule") +
+  ylab("Percentage of clicks on familiar object") +
+  scale_fill_manual(values = group.colours) +
+  theme_tufte_revised()
+dev.off()
+
 
 
 # LOGISTIC REGRESSION ####
@@ -106,6 +143,10 @@ contrasts(data$context) = c(1,0)
 data.glm1 = glmer(fam ~ rule*context + (rule*context|subject_id) + (1+context|word), data=data, family="binomial", control=glmerControl('bobyqa'))
 summary(data.glm1)
 
+# Releveling of rule baseline:
+contrasts(data$rule)    = c(0,1)
+data.glm2 = glmer(fam ~ rule*context + (rule*context|subject_id) + (1+context|word), data=data, family="binomial", control=glmerControl('bobyqa'))
+summary(data.glm2)
 
 # SIGNIFICANCE TESTS:
 
